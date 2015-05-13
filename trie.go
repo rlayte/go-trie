ea43d745ed4a6c2de83bf99e3254af8c857319fe
Rich Layte <rich.layte@gmail.com>
2015-05-13 14:33:22 +0100

    Improve tests

--- trie.go
+++ trie.go
@@ -1,46 +1,88 @@
 package trie
 
 type Trie struct {
-	value      string
+	Value      interface{}
 	terminated bool
-	children   map[string]Trie
+	children   map[string]*Trie
 }
 
-func NewTrie() Trie {
-	return Trie{children: map[string]Trie{}}
+func NewTrie() *Trie {
+	return &Trie{children: map[string]*Trie{}}
 }
 
-func getHeadTail(s string) (string, string) {
-	head := string(s[0])
-	tail := string(s[1:])
-
-	return head, tail
-}
-
-func (t *Trie) add(s string) {
-	if len(s) == 0 {
+func (t *Trie) Add(k string) *Trie {
+	if len(k) == 0 {
 		t.terminated = true
+		return t
+	}
+
+	head := string(k[0])
+	tail := string(k[1:])
+
+	child, ok := t.children[head]
+
+	if !ok {
+		child = NewTrie()
+		t.children[head] = child
+	}
+
+	return child.Add(tail)
+}
+
+func (t *Trie) removeChild(k string, ancestor *Trie, c string) {
+	if len(k) == 0 {
+		t.terminated = false
+
+		if len(t.children) == 0 {
+			delete(ancestor.children, c)
+		}
+
 		return
 	}
 
-	head, tail := getHeadTail(s)
-
-	child := NewTrie()
-	child.value = head
-	child.add(tail)
-	t.children[head] = child
-}
-
-func (t *Trie) contains(s string) bool {
-	if len(s) == 0 {
-		return t.terminated
-	}
-
-	head, tail := getHeadTail(s)
+	head := string(k[0])
+	tail := string(k[1:])
 
 	if child, ok := t.children[head]; ok {
-		return child.contains(tail)
+		if len(t.children) > 1 {
+			ancestor = t
+			c = head
+		}
+
+		child.removeChild(tail, ancestor, c)
+	}
+}
+
+func (t *Trie) Remove(k string) {
+	head := string(k[0])
+	t.removeChild(k, t, head)
+}
+
+func (t *Trie) Get(k string) (*Trie, bool) {
+	if len(k) == 0 {
+		return t, t.terminated
 	}
 
-	return false
+	head := string(k[0])
+	tail := string(k[1:])
+
+	if child, ok := t.children[head]; ok {
+		return child.Get(tail)
+	}
+
+	return t, false
+}
+
+func (t *Trie) Children(prefix string) []string {
+	children := []string{}
+
+	for key, child := range t.children {
+		if child.terminated {
+			children = append(children, prefix+key)
+		}
+
+		children = append(children, child.Children(prefix+key)...)
+	}
+
+	return children
 }
--- trie_test.go
+++ trie_test.go
@@ -2,24 +2,59 @@ package trie
 
 import "testing"
 
-func TestContains(t *testing.T) {
+func TestTrie(t *testing.T) {
 	trie := NewTrie()
-	trie.add("Hello")
-	trie.add("World")
 
-	if !trie.contains("Hello") {
-		t.Error("Should have key 'Hello'")
+	keys := []string{"Hello", "World", "Help", "Hell"}
+	missing := []string{"Hel", "Nope"}
+
+	for _, key := range keys {
+		trie.Add(key)
 	}
 
-	if !trie.contains("World") {
-		t.Error("Should have key 'World'")
+	for _, key := range keys {
+		_, ok := trie.Get(key)
+
+		if !ok {
+			t.Error("Missing key:", key)
+		}
 	}
 
-	if trie.contains("Hel") {
-		t.Error("Should not contain key 'Hel'")
+	for _, key := range missing {
+		_, ok := trie.Get(key)
+
+		if ok {
+			t.Error("Should not have key:", key)
+		}
 	}
 
-	if trie.contains("Skepta") {
-		t.Error("Should not contain 'Skepta'")
+	child, _ := trie.Get("Hello")
+	child.Value = "Updated"
+
+	if child, _ := trie.Get("Hello"); child.Value != "Updated" {
+		t.Error("Hello should now equal 100 but is", child.Value)
+	}
+
+	children := trie.Children("")
+
+	if len(children) != 4 {
+		t.Error("There should be 4 children. Actually:", len(children))
+	}
+
+	h, _ := trie.Get("H")
+	nested := h.Children("")
+
+	if len(nested) != 3 {
+		t.Error("There should be 2 children from 'H'. Actually:", len(nested))
+	}
+
+	trie.Remove("Hell")
+
+	if _, ok := trie.Get("Hell"); ok {
+		t.Error("Hell should have been removed")
+	}
+
+	if _, ok := trie.Get("Hello"); !ok {
+		t.Error("Hello should not have been removed")
 	}
 }
